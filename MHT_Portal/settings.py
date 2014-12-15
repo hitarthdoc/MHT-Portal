e41eceadad5832e52e046423888bc2e0f82496e2
Parth Gudhka <93parth@gmail.com>
2014-12-15 13:44:46 +0530

    Removes test files and adds a base_site.html

--- MHT_Portal/settings.py
+++ MHT_Portal/settings.py
@@ -126,7 +126,7 @@ ROOT_URLCONF = 'MHT_Portal.urls'
 WSGI_APPLICATION = 'MHT_Portal.wsgi.application'
 
 TEMPLATE_DIRS = (
-    'E:/MHT Portal/mpn/templates/admin'
+    '/MHT_Portal/templates/'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
