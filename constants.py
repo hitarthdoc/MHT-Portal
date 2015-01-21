cb8190a92449e7f48048cb2da83bb80f61fc3242
Parth Gudhka <93parth@gmail.com>
2015-01-21 15:10:33 +0530

    Some additions to the constants file

--- constants.py
+++ constants.py
@@ -7,3 +7,13 @@ EVENT_CATEGORY_CHOICES = ((0, 'GNC Day'),
                           (5, 'Parayan'),
                           (6, 'Janma Jayanti'),
                           (7, 'Picnic'))
+
+# These Constants have not been imported into profile/models but have been kept
+# for documentation. To change these, go to the original file for 
+# Edu_choices = (('school', "School"),
+#                ('college', "College"))
+
+# ATTENDED_DETAILS = ((1, 'All Days'), (2, 'Partial Days'))
+
+# GENDER_CHOICES = (('male', "Male"),
+#                   ('female', "Female"))
