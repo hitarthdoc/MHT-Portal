f72e5d5b135dbde49e089587c7081246c53517d3
Parth Gudhka <93parth@gmail.com>
2015-01-24 14:47:12 +0530

    Revert "Resets Membershipinline position to below Jobsinline"
    This reverts commit 77662974e6724b46d5585aeef625083be854ff72.

--- profile/admin.py
+++ profile/admin.py
@@ -45,7 +45,6 @@ class RequiredFormSet(forms.models.BaseInlineFormSet):
     def __init__(self, *args, **kwargs):
         super(RequiredFormSet, self).__init__(*args, **kwargs)
         for form in self.forms:
-            print form.instance.is_active
             form.empty_permitted = False
 
 
@@ -182,12 +181,12 @@ class profileAdmin(admin.ModelAdmin):
     list_filter = ('first_name', 'hobby')
     search_fields = ('first_name', 'last_name',)
     inlines = [
+        YMHTMembershipInline,
         YMHTMobileInline,
         YMHTEmailInline,
         YMHTAddressInline,
         YMHTEducationInline,
         YMHTJobInline,
-        YMHTMembershipInline,
         GlobalEventSewaDetailsInline,
         LocalEventSewaDetailsInline,
         GNCSewaDetailsInline,
