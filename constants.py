3e71d8de5c87ef71f5b66ac3f4485d930169e9c4
Parth Gudhka <93parth@gmail.com>
2015-02-14 23:19:16 +0530

    moved return_status and global_get_readonly_fields funcs from Sessions.admin to constants

--- constants.py
+++ constants.py
@@ -1,3 +1,32 @@
+import datetime
+YEAR_CHOICES = []
+for r in range(1980, (datetime.datetime.now().year+1)):
+    YEAR_CHOICES.append((r,r))
+
+def return_status(request, obj=None):
+    if request.user.is_superuser:
+        return "Superuser"
+    if obj:
+        if (obj.approved is True) and (request.user != obj.created_by):
+            return "Other User"
+        else:
+            return "Center Coordinator"
+    else:
+        return "New Object"
+
+
+def global_get_readonly_fields(caller, request, obj=None):
+    user_is_coord_of_current_center = False
+    status = return_status(request, obj)
+    result = {
+            'Superuser': [],
+            'Other User': caller.readonly_fields + caller.fields,
+            'Center Coordinator': [],
+            'New Object': [],
+        }[status]
+    return result
+
+
 # Currently not used in Profile events but kept as reference 
 EVENT_CATEGORY_CHOICES = ((0, 'GNC Day'),
                           (1, 'Summer Camp'),
@@ -8,10 +37,11 @@ EVENT_CATEGORY_CHOICES = ((0, 'GNC Day'),
                           (6, 'Janma Jayanti'),
                           (7, 'Picnic'))
 
-
 PARTICIPANT_ROLE_LEVEL = 1
 HELPER_ROLE_LEVEL = 2
 COORD_ROLE_LEVEL = 3
+
+
 # These Constants have not been imported into profile/models but have been kept
 # for documentation. To change these, go to the original file for 
 # Edu_choices = (('school', "School"),
